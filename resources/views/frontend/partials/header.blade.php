4cc5be10fdf752127148807455cd3c05e0037871
lesi20061998@gmail.com <lesi20061998@gmail.com>
2026-08-07 17:04:21 +0700

    1

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -88,27 +88,18 @@
 
 <!-- Header -->
 @php
-    // Kiểm tra xem header có trong DB không
-    $dbHeader = \App\Models\Post::where('post_type', 'header')->where('slug', $headerStyle)->first();
+    // Fallback to style-1 if selected style doesn't exist
+    $headerViewPath = 'frontend.partials.headers.' . $headerStyle;
+    if (!view()->exists($headerViewPath)) {
+        $headerViewPath = 'frontend.partials.headers.style-1';
+    }
 @endphp
-
-@if($dbHeader)
-    {!! $dbHeader->content !!}
-@else
-    @php
-        // Fallback to style-1 if selected style doesn't exist
-        $headerViewPath = 'frontend.partials.headers.' . $headerStyle;
-        if (!view()->exists($headerViewPath)) {
-            $headerViewPath = 'frontend.partials.headers.style-1';
-        }
-    @endphp
-    @include($headerViewPath, [
-        'headerBg' => $headerBg,
-        'headerText' => $headerText,
-        'logo' => $logo,
-        'siteName' => $siteName,
-        'hotline' => $hotline,
-        'navMenu' => $navMenu,
-        'projectCode' => $projectCode
-    ])
-@endif
+@include($headerViewPath, [
+    'headerBg' => $headerBg,
+    'headerText' => $headerText,
+    'logo' => $logo,
+    'siteName' => $siteName,
+    'hotline' => $hotline,
+    'navMenu' => $navMenu,
+    'projectCode' => $projectCode
+])
