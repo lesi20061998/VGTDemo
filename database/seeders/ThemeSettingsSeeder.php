abba5c5e3761e125f7e4baa3be32ad4dcfa974bf
lesi20061998@gmail.com <lesi20061998@gmail.com>
2026-08-10 14:10:29 +0700

    feat: implement watermarking system, media management tools, and project configuration module

--- database/seeders/ThemeSettingsSeeder.php
+++ database/seeders/ThemeSettingsSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Setting;
 
 class ThemeSettingsSeeder extends Seeder
 {
@@ -12,6 +12,17 @@ class ThemeSettingsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $settings = [
+            'theme_primary_color' => '#98191F',
+            'theme_secondary_color' => '#1F2937',
+            'theme_text_color' => '#374151',
+            'theme_bg_color' => '#FFFFFF',
+            'theme_font_family' => "'Poppins', sans-serif",
+            'theme_heading_font_family' => "'Montserrat', sans-serif",
+        ];
+
+        foreach ($settings as $key => $value) {
+            Setting::set($key, $value, 'theme');
+        }
     }
 }
